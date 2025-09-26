97d4528362455bcb609a94d3a0c3d01b84f06d31
quest82 <awoniyioluseyi@gmail.com>
2025-09-26 13:11:19 +0100

    Completed JS functionality and worked on the README file

--- 01_Results_Summary_Component/script.js
+++ 01_Results_Summary_Component/script.js
@@ -1,14 +1,15 @@
 "use strict";
 
-const overAllScore = document.querySelector(
-	".result__total__bubble__info__score"
-);
+// Setting up the DOM to interact with HTML elements
+const overAllScore = document.querySelector(".result__total__bubble__info__score");
 const memoryScore = document.querySelector(".memory");
 const reactionScore = document.querySelector(".reaction");
 const verbalScore = document.querySelector(".verbal");
 const visualScore = document.querySelector(".visual");
 const button = document.querySelector(".btn");
-const jsonData = `[
+
+// Setting up the JSON data to be used in the UI
+const jsonData = `[ 
   {
     "category": "Reaction",
     "score": 80,
@@ -31,15 +32,14 @@ const jsonData = `[
   }
 ]`;
 
-const convertedData = JSON.parse(jsonData);
+const convertedData = JSON.parse(jsonData); // Convert JSON string to iterable data type
 
-button.addEventListener('click', e => {
-    dataInjector()
-    setTimeout(scoreCounter, 400)
-    
-})
+button.addEventListener("click", (e) => { 
+	dataInjector();
+	setTimeout(scoreCounter, 400);
+}); // Launches the needed functionality once the button is clicked
 
-function dataInjector () {
+function dataInjector() {
 	for (let a of convertedData) {
 		if (a.category === "Memory") {
 			memoryScore.textContent = a.score;
@@ -51,15 +51,24 @@ function dataInjector () {
 			visualScore.textContent = a.score;
 		}
 	}
-};
+} // This function add the data to the test scores
 
-function scoreCounter () {
-    let count = 0
-    for (let a of convertedData) {
-        let dataNum = parseInt(a.score, 10)
-        count+=dataNum
-    }
-    let avg = Math.floor(count / convertedData.length)
-    overAllScore.textContent = avg
+function scoreCounter() {
+	let count = 0;
+	let start = 0;
+	for (let a of convertedData) {
+		let dataNum = parseInt(a.score, 10);
+		count += dataNum;
+	}
+	let avg = Math.floor(count / convertedData.length); // This code block calculates the average score
+
+	
+	let countDown = setInterval(() => {
+    overAllScore.textContent = start;
+		if (start === avg) {
+			clearInterval(countDown)
+		} else {
+			start++;
+		}
+	}, 100); // This code block creates a count up from 0 to average
 }
-
