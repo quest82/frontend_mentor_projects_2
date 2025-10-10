5bdb153751cb61f30ff0bb45bad701e14f0a8480
quest82 <awoniyioluseyi@gmail.com>
2025-10-10 13:43:22 +0100

    Finished the product. Found away around the animation.

--- 04_FAQ_Accordion_Main/script.js
+++ 04_FAQ_Accordion_Main/script.js
@@ -1,19 +1,18 @@
 const openBtn = document.querySelectorAll(".faq__img--open");
 const closeBtn = document.querySelectorAll(".faq__img--close");
 const answers = document.querySelectorAll(".faq__answer");
+const frequentlyAnsweredQuestions = document.querySelectorAll(".faq");
 
-openBtn.forEach((x, index) => {
+frequentlyAnsweredQuestions.forEach((x, index) => {
 	x.addEventListener("click", () => {
 		answers[index].classList.toggle("show");
-		x.style.display = "none";
-		closeBtn[index].style.display = "block";
-	});
-});
-
-closeBtn.forEach((x, index) => {
-	x.addEventListener("click", () => {
-		answers[index].classList.toggle("close");
-		x.style.display = "none";
-		openBtn[index].style.display = "block";
+		console.log(answers[index].classList);
+		if (answers[index].classList.contains("show")) {
+			openBtn[index].style.display = "none";
+			closeBtn[index].style.display = "block";
+		} else {
+			openBtn[index].style.display = "block";
+			closeBtn[index].style.display = "none";
+		}
 	});
 });
